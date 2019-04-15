daa80f4e3d2ccb8ff14b60854ea69726e054c811
Jon Knight <jon@metrixapps.com>
2019-04-14 20:04:29 -0700

    Create News DB Provider and needed model methods

---
+++ lib/src/resources/news_db_provider.dart
@@ -0,0 +1,60 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import 'package:tech_pulse/src/models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    Directory documentsDir = await getApplicationDocumentsDirectory();
+    final path = join(documentsDir.path, "items.db");
+
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time, INTEGER,
+            text TEXT,
+            parent INTEGER,
+            kids BLOB,
+            dead INTEGER,
+            deleted INTEGER,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
+      }
+    );
+  }
+
+  fetchIem(int id) async {
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+
+    if(maps.length > 0){
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item){
+    return db.insert("Items", item.toMapForDB());
+  }
+
+}
